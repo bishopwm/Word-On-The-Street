a68a8a4db4398891696b1df0c7e7025d82432f9f
William Bishop <bishopwm@g.cofc.edu>
2015-08-05 16:39:12 +0200

    herokpoo

--- app/services/insta_searcher.rb
+++ app/services/insta_searcher.rb
@@ -12,7 +12,6 @@ class InstaSearcher
 	  latitude = coordinates[:latitude]
 	  longitude = coordinates[:longitude]
 	  content_request = HTTParty.get("https://api.instagram.com/v1/media/search?lat=#{latitude}&lng=#{longitude}&access_token=#{access_token}&tag=#{tag}")
-	  # content_request = HTTParty.get("https://api.instagram.com/v1/media/search?tag=russian&")
 	  							  
 		content_request['data'].each do |insta_info|
 
@@ -25,7 +24,6 @@ class InstaSearcher
 	
 		end #if content_request["data"]
 		@insta_contents
-		fail
 
 	end
 end
