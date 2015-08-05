ab08fcfa3c18afc8fe6ab8d69311915a6699da37
William Bishop <bishopwm@g.cofc.edu>
2015-08-05 16:01:08 +0200

    heroku fix

--- app/services/insta_searcher.rb
+++ app/services/insta_searcher.rb
@@ -11,7 +11,7 @@ class InstaSearcher
 	  tag = parameters[:term]
 	  latitude = coordinates[:latitude]
 	  longitude = coordinates[:longitude]
-	  content_request = HTTParty.get("https://api.instagram.com/v1/media/search?lat=41.3916642&lng=2.1768958&access_token=#{access_token}&tag=#{tag}")
+	  content_request = HTTParty.get("https://api.instagram.com/v1/media/search?lat=#{latitude}&lng=#{longitude}&access_token=#{access_token}&tag=#{tag}")
 	  							  
 		content_request['data'].each do |insta_info|
 
