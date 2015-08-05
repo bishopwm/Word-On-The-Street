65f71c5d782ab4856167c52fc94fd86876481ea5
William Bishop <bishopwm@g.cofc.edu>
2015-08-05 15:29:36 +0200

    heroku bullshit

--- app/services/insta_searcher.rb
+++ app/services/insta_searcher.rb
@@ -5,8 +5,6 @@ class InstaSearcher
 	  client_secret = ENV["instagram_client_secret"]
 
 
-	  # auth_request = HTTParty.get("https://instagram.com/oauth/authorize/?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=token")
-
 	  @insta_contents = []	
 
 	  access_token = ENV["instagram_access_token"]
@@ -14,9 +12,7 @@ class InstaSearcher
 	  latitude = coordinates[:latitude]
 	  longitude = coordinates[:longitude]
 	  content_request = HTTParty.get("https://api.instagram.com/v1/media/search?lat=41.3916642&lng=2.1768958&access_token=#{access_token}&tag=#{tag}")
-	  								  # https://api.instagram.com/v1/media/search?lat=41.3916642&lng=2.1768958&access_token=#{access_token}&tag=#{tag}
-	  								  # https://api.instagram.com/v1/tags/{tag-name}/media/lat=#{latitude}&lng=#{longitude}&access_token=#{access_token}
-	  								  # https://api.instagram.com/v1/media/search?lat=#{latitude}&lng=#{longitude}&access_token=#{access_token}&tag=#{tag}
+	  								  
 		content_request["data"].each do |datum|
 
 		  @insta_contents << {
@@ -25,7 +21,7 @@ class InstaSearcher
 		  	'coordinates' => [datum['location']['latitude'],datum['location']['longitude']],
 		  	'location_name' => datum['location']['name']
 		  }
-		end if content_request["data"]
+		end #if content_request["data"]
 		@insta_contents
 
 	end
