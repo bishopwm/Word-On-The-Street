bcf365a4f8973a5ba5613b2ee7153ed9e86dd2da
William Bishop <bishopwm@g.cofc.edu>
2015-08-24 11:36:33 -0400

    removed non-utilized code(like the facebook like button)

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -5,8 +5,4 @@ class CallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user
     end
 
-    # def facebook
-    #     @user = User.from_omniauth(request.env["omniauth.auth"])
-    #     sign_in_and_redirect @user
-    # end
 end
--- app/services/facebook_searcher.rb
+++ app/services/facebook_searcher.rb
@@ -2,14 +2,14 @@ class FacebookSearcher
 
 	def get_facebook_results(user_city, parameters)
 		query = parameters[:term]
-	  current_city = user_city 
+	  	current_city = user_city 
 
 	  	oauth_token = User.last.access_token
 	 	app_secret = ENV["facebook_app_secret"]
 
 	 	@facebook_events = []
 
-	  facebook = Koala::Facebook::API.new(oauth_token, app_secret)
+	  	facebook = Koala::Facebook::API.new(oauth_token, app_secret)
 		responses = facebook.get_object("search?fields=name&type=event&q=#{query}+#{current_city}")
 
 		responses.each do |facebook_event|
@@ -18,6 +18,7 @@ class FacebookSearcher
 				'id' => facebook_event["id"]
 			}
 		end
+		
 		@facebook_events
 
 	end
